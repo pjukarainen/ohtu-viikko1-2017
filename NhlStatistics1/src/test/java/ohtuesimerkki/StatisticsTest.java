fffa4bda22a44c7d53597bde38e5421a86adc8ef
Pietari Jukarainen <pjukarainen@gmail.com>
2017-11-05 16:57:11 +0200

    toimi

--- NhlStatistics1/src/test/java/ohtuesimerkki/StatisticsTest.java
+++ NhlStatistics1/src/test/java/ohtuesimerkki/StatisticsTest.java
@@ -5,6 +5,8 @@
  */
 package ohtuesimerkki;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -17,29 +19,39 @@ import static org.junit.Assert.*;
  * @author jappappi
  */
 public class StatisticsTest {
-    
+
+    Reader readerStub = new Reader() {
+
+        public List<Player> getPlayers() {
+            ArrayList<Player> players = new ArrayList<Player>();
+
+            players.add(new Player("Semenko", "EDM", 4, 12));
+            players.add(new Player("Lemieux", "PIT", 45, 54));
+            players.add(new Player("Kurri", "EDM", 37, 53));
+            players.add(new Player("Yzerman", "DET", 42, 56));
+            players.add(new Player("Gretzky", "EDM", 35, 89));
+
+            return players;
+        }
+    };
+
     public StatisticsTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
 
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
 }
